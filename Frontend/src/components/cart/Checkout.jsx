9974ff77ff1eb03044f0e7a1b0273786349a21c0
Ankit Anurag <anuragankit01@gmail.com>
2025-10-05 15:00:40 +0530

    changed an import in app.jsx

--- Frontend/src/components/cart/Checkout.jsx
+++ Frontend/src/components/cart/Checkout.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import PayPalButton from "./PaypalButton";
+import PayPalButton from "./PayPalButton";
 import { useDispatch, useSelector } from "react-redux";
 import { createCheckout } from "../../redux/slices/checkoutSlice";
 
